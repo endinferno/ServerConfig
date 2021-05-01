8a080ba818513a810cd03cc200d11d97ff33886e
endinferno <a9214863@sina.com>
2021-05-02 01:36:10 +0800

    feat: add support of trash or trash-cli

--- install-zsh.sh
+++ install-zsh.sh
@@ -2,7 +2,18 @@
 
 sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-mv ./zshrc-config ~/.zshrc
+cp ./zshrc-config ~/.zshrc
+if["$(uname)"=="Darwin"];then
+	# MacOS
+	brew install trash
+	echo "alias rm=\"trash\"" >> ~/.zshrc
+elif["$(expr substr $(uname -s) 1 5)"=="Linux"];then
+	# GNU/Linux
+	sudo apt install trash-cli
+	echo "alias rm=\"trash-put\"" >> ~/.zshrc
+elif["$(expr substr $(uname -s) 1 10)"=="MINGW32_NT"];then
+	# Windows
+fi
 cd ~/.oh-my-zsh/plugins
 for plugin in 'colored-man-pages' 'common-aliases' 'extract' 'git' 'sudo' 'z' 'fz' 'zsh-autosuggestions' 'zsh-syntax-highlighting' 'zsh-history-substring-search'
 do
@@ -11,35 +22,34 @@ do
 		echo "No Such Plugin"
 		echo "Install " $plugin
 		case $plugin in
-			'colored-man-pages')  
+			'colored-man-pages')
 				;;
-			'common-aliases')  
+			'common-aliases')
 				;;
-			'extract')  
+			'extract')
 				;;
-			'git')  
+			'git')
 				;;
-			'sudo')  
+			'sudo')
 				;;
-			'z')  
+			'z')
 				;;
-			'fz')  
+			'fz')
 				git clone https://github.com/changyuheng/fz.git
 				;;
-			'zsh-autosuggestions') 
+			'zsh-autosuggestions')
 				git clone  https://github.com/zsh-users/zsh-autosuggestions.git
 				;;
-			'zsh-syntax-highlighting')  
+			'zsh-syntax-highlighting')
 				git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 				;;
-			'zsh-history-substring-search')  
+			'zsh-history-substring-search')
 				git clone https://github.com/zsh-users/zsh-history-substring-search.git
 				;;
-			*)  
+			*)
 				echo 'No such plugin'
 				;;
 		esac
 	fi
 done
-
 

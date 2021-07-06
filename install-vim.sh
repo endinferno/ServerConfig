698eb195e95311c549a79658fe64d2ffccd68144
Edward C <540338575@qq.com>
2021-07-06 12:45:39 +0800

    Update install-vim.sh
    Modify dependent libraries

--- install-vim.sh
+++ install-vim.sh
@@ -10,7 +10,7 @@ vim_soft_name=$(dpkg -l | grep vim | awk '{print $2}')
 sudo dpkg -P $vim_soft_name
 
 CPU_NUM=$(cat /proc/cpuinfo | grep "MHz" | wc -l)
-sudo apt install python3-dev ruby-dev lua5.1 lua5.1-dev
+sudo apt install python3-dev ruby-dev lua5.1 libncurses-dev
 git clone https://github.com/vim/vim.git
 cd vim
 ./configure --enable-fontset --enable-cscope --enable-terminal --enable-multibyte --enable-python3interp=yes --with-python3-config-dir=$1 --enable-luainterp=yes --enable-rubyinterp=yes --enable-perlinterp=yes

cdc9ccbcbfc2c3aee172da7431322092553492c8
Shaofeng Cen <censhaofeng@sjtu.edu.cn>
2024-04-08 21:44:09 +0800

    fix: update start-clash.sh script

--- clash/start-clash.sh
+++ clash/start-clash.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-FILE=$HOME/clash/clash.log
-if [ -f "$FILE" ]; then
-    rm $FILE
+CLASH_BASE=$HOME/clash
+FILE=$CLASH_BASE/clash.log
+clash_pid_list=$(ps -aux | grep -v grep | grep clash | grep yaml | awk -F ' ' '{print $2}')
+if [[ ! -z "$clash_pid_list" ]]
+then
+	kill -9 $clash_pid_list
 fi
-nohup $PWD/clash/clash -f $PWD/clash/ByWave.yaml > $PWD/clash/clash.log 2>&1 &
+if [ -f "$FILE" ]; then
+	rm $FILE
+fi
+nohup $CLASH_BASE/clash -f $CLASH_BASE/ByWave.yaml > $CLASH_BASE/clash.log 2>&1 &

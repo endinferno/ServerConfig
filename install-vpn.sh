a9f039c34d523104e55636d1a2c1a63c45a6b98e
Shaofeng Cen <censhaofeng@sjtu.edu.cn>
2024-05-31 14:48:16 +0800

    fix: update install vpn script

--- install-vpn.sh
+++ install-vpn.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-VPN_BASE=$HOME
-VPN_FOLDER_NAME=clash
-VPN_HOME=$VPN_BASE/$VPN_FOLDER_NAME
-if [ ! -d "$VPN_HOME" ]
-then
-    mkdir $VPN_HOME
-fi
-cp ./clash/clash $VPN_HOME
-cp ./clash/start_clash.sh $VPN_HOME
-cp ./clash/Country.mmdb $VPN_HOME
-scp root@121.5.185.52:~/file/config.yaml $VPN_HOME
-chmod +x $VPN_HOME/clash
-chmod +x $VPN_HOME/start_clash.sh
+CheckDirExist()
+{
+    if [ ! -d $1 ];then
+        echo "$1 Doesn't exist, create!"
+        mkdir -p $1
+    else
+        echo "$1 exist, pass!"
+    fi
+}
+
+CopyVPNFile()
+{
+    echo "Copy VPN files to $1"
+    cp ./clash/clash $1
+    cp ./clash/start-clash $1
+    cp ./clash/Country.mmdb $1
+    cp ./clash/Country.mmdb $2
+    chmod +x $1/clash
+    chmod +x $1/start-clash
+}
+
+SOFTWARE_BASE=$HOME/software
+CheckDirExist $SOFTWARE_BASE
+VPN_HOME=$SOFTWARE_BASE/clash
+CheckDirExist $VPN_HOME
+VPN_CONFIG_HOME=$HOME/.config/clash
+CheckDirExist $VPN_CONFIG_HOME
+CopyVPNFile $VPN_HOME $VPN_CONFIG_HOME
 echo "alias start-proxy=\"export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890\"
 alias close-proxy=\"unset https_proxy && unset http_proxy && unset all_proxy\"" >> ~/.bashrc
-/bin/bash $VPN_HOME/start-clash.sh
 source ~/.bashrc
